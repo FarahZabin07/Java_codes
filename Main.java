f64272d1a7adbc7ae802c387dc18f402f61cc161
Farah Zabin <farahzabin02@gmail.com>
2024-09-25 22:06:05 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
-package animals;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-    Mammal mammal=new Mammal("Lion",4,"Golden");
-    Bird bird=new Bird("Eagle",3,2.5f);
-    Reptile reptile=new Reptile("Turtle",5,"Green");
-
-        System.out.println("Bird");
-        bird.sleep(6);
-        bird.sleep(8);
-        System.out.println("Mammal");
-        mammal.eat();
-        System.out.println("Reptile");
-        reptile.crawl();
-
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
